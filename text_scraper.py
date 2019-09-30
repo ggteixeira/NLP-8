ada9167843475c48474001a9fd2b1867e2173e3f
guiemi <guiemi.teixeira@gmail.com>
2019-09-30 08:59:33 -0300

    Adiciona um set dinamico ao código:
    salva em uma variável (um set) todos os parentes de cada token
    encontrado no texto parseado pelo beautifulsoup.

--- text_scraper.py
+++ text_scraper.py
@@ -1,6 +1,7 @@
 import requests
 
-url = "https://manualdousuario.net/celular-pirata-nao-homologado-bloqueio/"
+url = "https://www.infowester.com/guiahdinic.php"
+# url = input("URL: ")
 res = requests.get(url)
 html_page = res.content
 
@@ -10,45 +11,10 @@ soup = BeautifulSoup(html_page, "html.parser")
 
 text = soup.findAll(text=True)
 
-# print(set([t.parent.name for t in text]))
+# set([t.parent.name for t in text])
 output = " "
-blacklist = [
-    "link",
-    "header",
-    "ul",
-    "a",
-    "[document]",
-    "form",
-    "option",
-    "time",
-    "head",
-    "html",
-    "h3",
-    "span",
-    "b",
-    "strong",
-    "img",
-    "em",
-    "li",
-    "h2",
-    "select",
-    "nav",
-    "h1",
-    "article",
-    "body",
-    "title",
-    "label",
-    "input",
-    "div",
-    "script",
-    "i",
-    "svg",
-    "footer",
-    "ol",
-    "main",
-    "meta",
-]
-
+blacklist = set([t.parent.name for t in text])
+blacklist.remove('p')
 for t in text:
     if t.parent.name not in blacklist:
         output += f"{t}"
