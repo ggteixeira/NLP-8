be3323975d991165f366561ac9e4d8dda210dd18
guiemi <guiemi.teixeira@gmail.com>
2019-09-30 10:22:48 -0300

    Comenta o código e o reorganiza

--- text_scraper.py
+++ text_scraper.py
@@ -1,22 +1,28 @@
+# Importação de módulos
 import requests
-
-url = "https://www.infowester.com/guiahdinic.php"
-# url = input("URL: ")
-res = requests.get(url)
-html_page = res.content
-
 from bs4 import BeautifulSoup
 
+#  Interação com o usuário
+url = input("Insira uma URL: ")
+
+# Faz um requeset da URL e a salva numa variável
+res = requests.get(url)
+
+# Baixa todo o conteúdo da página referente à URL
+html_page = res.content
+
+# Parseia o HTML, dando uma 'prettifycada' nele.
 soup = BeautifulSoup(html_page, "html.parser")
 
+# Limpa parcialmente o conteúdo, salvando apenas texto presente na página
 text = soup.findAll(text=True)
 
-# set([t.parent.name for t in text])
-output = " "
+output = str()
 blacklist = set([t.parent.name for t in text])
 blacklist.remove('p')
+
 for t in text:
     if t.parent.name not in blacklist:
         output += f"{t}"
 
-print(output)
+print(text)
