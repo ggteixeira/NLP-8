780eda053424a1bfcd27298900b4ba28459904ae
guiemi <guiemi.teixeira@gmail.com>
2019-09-30 22:42:27 -0300

    Modulariza o código

--- text_scraper.py
+++ text_scraper.py
@@ -3,26 +3,35 @@ import requests
 from bs4 import BeautifulSoup
 
 #  Interação com o usuário
-url = input("Insira uma URL: ")
-
+# url = input("Insira uma URL: ")
+url = "https://manualdousuario.net/sobre/"
 # Faz um requeset da URL e a salva numa variável
-res = requests.get(url)
+def get_page(url):
+    return requests.get(url)
 
 # Baixa todo o conteúdo da página referente à URL
-html_page = res.content
+def download_page(get_page):
+    return get_page.content
 
 # Parseia o HTML, dando uma 'prettifycada' nele.
-soup = BeautifulSoup(html_page, "html.parser")
+def html_parse(download_page):
+    return BeautifulSoup(download_page, "html.parser")
 
 # Limpa parcialmente o conteúdo, salvando apenas texto presente na página
-text = soup.findAll(text=True)
+def html_clean(html_parse):
+    return html_parse.findAll(text=True)
 
-output = str()
-blacklist = set([t.parent.name for t in text])
-blacklist.remove('p')
+# Cria dinamicamente uma lista de tags a serem ignoradas
+def set_blacklist(html_clean):
+    blacklist = set([token.parent.name for token in html_clean])
+    blacklist.remove('p')
+    return blacklist
 
-for t in text:
-    if t.parent.name not in blacklist:
-        output += f"{t}\n"
+def deep_clean(set_blacklist, html_clean):
+    output = str()
+    for token in html_clean:
+        if token.parent.name not in set_blacklist(html_clean):
+            output += f"{token }\n"
+    return output
 
-print(output)
+print(deep_clean(set_blacklist, html_clean(html_parse(download_page(get_page(url))))))
