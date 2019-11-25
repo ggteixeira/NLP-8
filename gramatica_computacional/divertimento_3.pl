95959efbf7ab0932c3deffb33374e6b07812491d
guiemi <guiemi.teixeira@gmail.com>
2019-11-25 16:15:44 -0300

    Modifica o código para que bolas pulem em ver de correrem

--- gramatica_computacional/divertimento_3.pl
+++ gramatica_computacional/divertimento_3.pl
@@ -3,5 +3,5 @@ sn(ANIMA) --> nome(ANIMA).
 sv(ANIMA) --> verbo(ANIMA).
 nome(anim) --> [ana] | [joana] | [beatriz] | [fabio] | [fernando] | [menino] | [garoto] | [menina].
 nome(inanim) --> [bola].
-verbo(anim) --> [morre] | [anda] | [pula].
-verbo(inanim) --> [corre].
+verbo(anim) --> [morre] | [anda] | [pula] | [corre].
+verbo(inanim) --> [pula].
