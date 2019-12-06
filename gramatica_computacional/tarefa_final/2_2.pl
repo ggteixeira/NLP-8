fe3e32d4441eea4f6a86bbaffb85b02d80fc72ef
guiemi <guiemi.teixeira@gmail.com>
2019-12-06 18:36:29 -0300

    Resolve a tarefa 2

--- gramatica_computacional/tarefa_final/2_2.pl
+++ gramatica_computacional/tarefa_final/2_2.pl
@@ -1,2 +1,20 @@
 % O nanocompósito possui uma área de superfície elevada.
 % Os nanocompósitos possuem uma área de superfície elevada.
+
+s --> noun_phrase(NUM), verb_phrase(NUM).
+noun_phrase(NUM) --> art(NUM), noun(NUM).
+verb_phrase(NUM) --> verb(NUM), art, prep_phrase.
+prep_phrase --> psubj, prep, pobj, adj.
+
+art(sing) --> [o].
+art(plur) --> [os].
+art --> [uma].
+noun(sing) --> [nanocomposito].
+noun(plur) --> [nanocompositos].
+psubj --> [area].
+pobj --> [superficie].
+verb(sing) --> [possui].
+verb(plur) --> [possuem].
+
+prep --> [de].
+adj --> [elevada].
